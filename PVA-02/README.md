354ac562d8f64a4cc82cc1b83b898e740c5368ca
Matthias Heimberg <matthias.heimberg@students.ffhs.ch>
2023-03-27 21:32:19 +0200

    correct data

---
+++ PVA-02/README.md
@@ -0,0 +1,7 @@
+# PVA-02
+## Vorgehen
+1. Erste Zeile aus Dateien '\PVA-02\Datensatz\CSVs\Datensatz-CorneliaIsenschmid.csv' und '\PVA-02\Datensatz\CSVs\Datensatz_VivianeRogenmoser.csv' manuell entfernt, da diese Zeilen keine Daten enthalten.
+2. Erste Spalte aus Datei '\PVA-02\Datensatz\CSVs\Datensatz_VivianeRogenmoser.csv' manuell entfernt, da diese keine Daten enthält
+3. Python Skript erstellt in welchem die folgenden Schritte nacheinander durchgeführt werden:
+   1. Leere Zeilen aus allen CSV Dateien entfernen (dies betrifft nur die Datei '\PVA-02\Datensatz\CSVs\Datensatz_OlgaEngelmann.csv' aber wäre manuell zu aufwändig)
+   2. 
--- PVA-02/cleanup-csv.py
+++ PVA-02/cleanup-csv.py
@@ -1,60 +1,78 @@
 import os
 import csv
 
-# Define a function to correct the name column
-def correct_name(name):
-    name = name.replace("minus", "-")
-    name = name.replace("kreuz", "x")
-    name = name.replace("plus", "+")
-    name = name.replace("gartenhag", "#")
-    name = name.replace("kreis", "o")
-    return name
+# Define the correction rules for symbols
+def substitute(symbol):
+    symbol = symbol.replace("minus", "-")
+    symbol = symbol.replace("kreuz", "x")
+    symbol = symbol.replace("plus", "+")
+    symbol = symbol.replace("gartenhag", "#")
+    symbol = symbol.replace("kreis", "o")
+    return symbol
 
-# Define a function to normalize the pixel values
-def normalize_pixel(pixel, max_pixel):
-    return float(pixel) / float(max_pixel)
-
-# Define a function to flip the binary values
-def flip_binary(pixel):
-    if int(pixel) > (n / 2):
-        return pixel
+# "normalize" to 1
+def substitute(number):
+    if number == "255":
+        return "1"
+    elif number == "True":
+        return "1"
+    elif number == "False":
+        return "0"
     else:
-        return str(1 - int(pixel))
+        return number
+    
+# Define the input and output directories
+input_dir = "./PVA-02/Datensatz/CSVs/"
+output_dir = "./PVA-02/Datensatz/CSVs/corrected/"
 
-# Define the paths to the input and output directories
-input_path = "./PVA-02/Datensatz/CSVs/"
-output_path = "./PVA-02/Datensatz/CSVs/corrected/"
-
-# Create the output directory if it does not exist
-if not os.path.exists(output_path):
-    os.makedirs(output_path)
-
-# Loop through each CSV file in the input directory
-for filename in os.listdir(input_path):
+# Loop through the input files
+for filename in os.listdir(input_dir):
+    # remove empty lines in the csv files
     if filename.endswith(".csv"):
-        # Open the input CSV file and create a new output file
-        with open(input_path + filename, "r") as csv_file, open(output_path + filename, "w", newline="") as out_file:
-            reader = csv.reader(csv_file)
-            writer = csv.writer(out_file)
+        print("Processing file " + filename)
+        with open(input_dir + filename, "r") as input_file:
+            lines = input_file.readlines()
+        lines = [line for line in lines if line.strip()]
+        with open(input_dir + filename, "w") as output_file:
+            output_file.writelines(lines)
+        # correct the symbols in the csv files
+        for i in range(len(lines)):
+                columns = lines[i].split(",")
+                columns[0] = substitute(columns[0])
+                lines[i] = ",".join(columns)
+        with open(input_dir + filename, "w") as output_file:
+            output_file.writelines(lines)
+        # normalize 255 -> 1
+        for i in range(len(lines)):
+            columns = lines[i].split(",")
+            for j in range(len(columns)):
+                columns[j] = substitute(columns[j])
+            lines[i] = ",".join(columns)
+        with open(input_dir + filename, "w") as output_file:
+            output_file.writelines(lines)
+        # count the occurence of 0 and 1
+        count_0 = 0
+        count_1 = 0
+        for line in lines:
+            columns = line.split(",")
+            for column in columns:
+                if column == "0":
+                    count_0 += 1
+                elif column == "1":
+                    count_1 += 1
+        # modify files with count(0) < count(1)
+        if count_0 < count_1:
+            for i in range(len(lines)):
+                columns = lines[i].split(",")
+                for j in range(len(columns)):
+                    if columns[j] == "0":
+                        columns[j] = "1"
+                    elif columns[j] == "1":
+                        columns[j] = "0"
+                lines[i] = ",".join(columns)
+        # write modified files to output directory
+        with open(output_dir + filename, "w") as output_file:
+            output_file.writelines(lines)
 
-            # Loop through each row in the CSV file
-            for row in reader:
-                # Skip empty rows or rows with fewer than 2 elements
-                if not row or len(row) < 2:
-                    continue
-
-                # Correct the name column
-                row[0] = correct_name(row[0])
-
-                # Normalize the pixel values and flip binary values
-                pixels = [p for p in row[1:] if p.isdigit()]
-                if not pixels:
-                    max_pixel = 1
-                else:
-                    max_pixel = max([int(p) for p in pixels])
-                n = len(row) - 1
-                row[1:] = [normalize_pixel(p, max_pixel) if p.isdigit() else (0 if p == "False" else 1) for p in row[1:]]
-                row[1:] = [flip_binary(p) for p in row[1:]]
-
-                # Write the row to the output file
-                writer.writerow(row)
+                    
+                    
